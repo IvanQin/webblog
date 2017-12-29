a066543c516c2432b9b55e4c7b33ba37061b546b
Qinyifan <yifanq@kth.se>
2017-12-28 23:03:10 -0800

    Mod: specified user schema restriction; change db operation to async

--- webblog/utils/dbHelper.js
+++ webblog/utils/dbHelper.js
@@ -12,39 +12,86 @@ const DEFAULT_DB_NAME = "";
 const HOST = "mongodb://127.0.0.1:27017/";
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [(email) => {
+            return /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(email)
+        }, "Invalid email"]
+    },
+    password: {
+        type: String,
+        validate: [(password) => {
+            return /.+/.test(password)
+        }, "password cannot be none!"]
+    }
 });
 
 const mapNameToSchema = {
-    "user":userSchema
+    "user": userSchema
 };
 /**
  *
- * @param dbRequestTemplate a json string
+ * @param dbRequestTemplate a JSON string, not a JSON object!
  * @constructor
  */
 const DBRequest = function (dbRequestTemplate) {
-    this.dbName = dbRequestTemplate.dbName ? dbRequestTemplate.dbName : DEFAULT_DB_NAME;
-    this.collectionName = dbRequestTemplate.collectionName;
-    this.operation = dbRequestTemplate.operation;
+    let dbReqObj = JSON.parse(dbRequestTemplate);
+    this.dbName = dbReqObj.dbName ? dbReqObj.dbName : DEFAULT_DB_NAME;
+    this.collectionName = dbReqObj.collectionName;
+    this.operation = dbReqObj.operation;
     this.schema = mapNameToSchema[this.collectionName]; // TODO: choose schema according to collection Name
-    this.document = null; // TODO
+    this.document = dbReqObj.document;
 };
+
+function dbDisconnect() {
+    return mongoose.disconnect();
+}
 
 /**
  * Insertion in mongodb
  * @param document document of mongodb
  * @param Model model of mongodb
  */
-function dbInsert(document,Model) {
-    Model.save(document,(err,res) => {
-        if (err){
+function dbInsert(document, Model) {
+    return new Promise((resolve, reject) => {
+        let model = new Model(document);
+        model.save((err, res) => {
+            if (err) {
+                sendErrorMessage();
+                //console.log(err);
+                reject("Error!");
+            }
+            //console.log(res);
+            dbDisconnect();
+            resolve("Success!");
+        }).then();
+    });
+    // Model.create(document, (err, res) => {
+    //     if (err) {
+    //         sendErrorMessage();
+    //         console.log(err);
+    //     }
+    //     console.log(res);
+    //     dbDisconnect();
+    // }).then();
+
+}
+
+function dbSearch(document, Model) {
+    Model.find(document, (err, res) => {
+        if (err) {
             sendErrorMessage();
             console.log(err);
         }
         console.log(res);
+        dbDisconnect();
     });
 }
 
@@ -53,12 +100,41 @@ function dbInsert(document,Model) {
  * @param dbRequestTemplate a json string
  */
 export function entrance(dbRequestTemplate) {
+    // return new Promise((resolve, reject) => {
+    //     let dbRequest = new DBRequest(dbRequestTemplate);
+    //     mongoose.connect(HOST + dbRequest.dbName);
+    //     let Model = mongoose.model(dbRequest.collectionName, dbRequest.schema);
+    //     switch (dbRequest.operation) {
+    //         case 0:
+    //             dbInsert(dbRequest.document, Model).then(res => {
+    //                 resolve(res);
+    //             }, err => {
+    //                 reject(err)
+    //             });
+    //             break; // 0 --> insert
+    //         case 1:
+    //             break; // 1 --> update
+    //         case 2:
+    //             dbSearch(dbRequest.document, Model);
+    //             break; // 2 --> search
+    //     }
+    // });
+
     let dbRequest = new DBRequest(dbRequestTemplate);
     mongoose.connect(HOST + dbRequest.dbName);
-    let Model = mongoose.model(dbRequest.collectionName,dbRequest.schema);
-    switch (dbRequest.operation){
-        case 0: dbInsert(dbRequest.document,Model);break; // 0 --> insert
-        case 1: break; // 1 --> update
+    let Model = mongoose.model(dbRequest.collectionName, dbRequest.schema);
+    let retPromise;
+    switch (dbRequest.operation) {
+        case 0:
+            retPromise = dbInsert(dbRequest.document, Model);
+            break; // 0 --> insert
+        case 1:
+            break; // 1 --> update
+        case 2:
+            dbSearch(dbRequest.document, Model);
+
+            break; // 2 --> search
+
     }
-    return Model;
+    return retPromise;
 }
