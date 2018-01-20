851f28e3c74de71ef58b62e0ac8c47c7ebac4ed7
Qinyifan <yifanq@kth.se>
2018-01-19 17:16:57 -0800

    Mod : configure the baseURL of axios

--- frontend/blog/src/main.js
+++ frontend/blog/src/main.js
@@ -6,10 +6,13 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
+
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.use(JSON)
+axios.defaults.baseURL = 'http://localhost:3000'
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
