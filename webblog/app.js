564f46378327fa8f0495f2912cc98d3671963fb8
Qinyifan <yifanq@kth.se>
2018-01-19 17:17:14 -0800

    Mod: add comments

--- webblog/app.js
+++ webblog/app.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/db', db);
+app.use('/db', db); // only accept post method
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
