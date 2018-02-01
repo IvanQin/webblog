5f354eeeaa895c51e021af6dcd5118e23d3a9808
Qinyifan <yifanq@kth.se>
2018-02-01 09:30:47 -0800

    Add: configure database template

---
+++ frontend/blog/src/utils/utils.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Qinyifan on 18/1/31.
+ */
+const DEFAULT_DB_NAME = "test";
+/**
+ * Generate template for db operation between frontend and backend
+ * @param opID: operation ID ( defined in webblog/utils/dbHelper.js )
+ * @param collectionName: the name of collection to operate in mongodb
+ * @param data: content
+ * @returns {{dbName: string, collectionName: *, operation: *, document: *, updateDoc: *}}
+ */
+export function getDbOperationTemplate(opID,collectionName,data){
+    let dbName = DEFAULT_DB_NAME;
+    let template = {
+        "dbName": dbName,
+        "collectionName": collectionName,
+        "operation": opID,
+        "document": data.document?data.document:{},
+        "updateDoc": data.updateDoc?data.updateDoc:{}
+    };
+    return template;
+};
+//module.exports = getDbOperationTemplate;
