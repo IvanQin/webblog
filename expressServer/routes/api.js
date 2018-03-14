f783b1f825b120473a9b0d046bead63290241ac3
Qinyifan <yifanq@kth.se>
2018-03-14 00:31:40 -0700

    Mod: finish api get-token and remain validate-token unchecked

--- expressServer/routes/api.js
+++ expressServer/routes/api.js
@@ -16,9 +16,24 @@ router.post('/get-token', (req, res, next) => {
     // use current Time and roomId
     let str = Date.now() + '&' + content['roomId'];
     let encodeStr = md5(str);
-    res.cookie('token', encodeStr, {maxAge: 3600*1000, httpOnly: true}); // expire time
-    res.send('Success');
+    res.cookie('token', encodeStr, {maxAge: 3600 * 1000, httpOnly: true}); // expire time
+    let insertTokenRequest = content['dbDocument'];
+    insertTokenRequest.document['token'] = encodeStr; // add newly generated token in it
+    dbHelper.entrance(JSON.stringify(insertTokenRequest)).then(resolve => {
+        res.send({'accessToken': encodeStr})
+    }, rej => res.send(rej));
 
 });
 
+router.post('/validate-token', (req, res, next)=>{
+    let content = req.body;
+    let validateTokenRequest = content['dbDocument'];
+    dbHelper.entrance(JSON.stringify(validateTokenRequest)).then(resolve => {
+        console.log(resolve);
+        if (JSON.stringify(resolve).length != 0) {
+            res.send({'status': 'OK'})
+        } // TODO change ok
+    }, rej => res.send(rej));
+
+});
 module.exports = router;
